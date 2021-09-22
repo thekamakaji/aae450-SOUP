4523db4bcf0f67853163ecba001086c212ede23d
vswamin1998 <91161213+vswamin1998@users.noreply.github.com>
2021-09-21 22:55:15 -0400

    Add files via upload
    All these files are currently in Octave and will need to be slightly modified for use in Matlab

---
+++ orbitShiftAN.m
@@ -0,0 +1,4 @@
+% Calculate the orbit shift in degrees longitude --> p_sat in days
+function [d_lambda] = orbitShiftAN(p_sat)
+  d_lambda = (-0.2507)*p_sat*(24*60); % degrees of shift per rev
+endfunction
